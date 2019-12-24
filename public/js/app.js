c97790cc44cbddea25ee3295d6efd3ceaa75e1a4
Bryan Top <Bryantop13262@GMAIL.COM>
2019-12-24 11:57:36 -0800

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = searchInput.value;
 
-    const  url = 'http://localhost:3000/weather?address=' + location;
+    const  url = '/weather?address=' + location;
 
     message1.textContent = 'loading...';
     message2.textContent = '';
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define path for express config
 const publicDirPath = path.join(__dirname, '../public');
@@ -75,6 +76,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("server started on port 3000");
+app.listen(port, () => {
+    console.log("server started on port " + port);
 });
